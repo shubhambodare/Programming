e151f7153ac4cfbbb9b405692f0b9d0e79f6ca2f
Shubham <Shubham@shubham>
2019-07-23 19:22:49 +0530

    mark comments in project

--- Hub/src/Multiple.java
+++ Hub/src/Multiple.java
@@ -1,44 +1,31 @@
-
+//Inheritance with multiple overloading
 class Parent{
 	
 	public void add(int a,int b) {
-		
-		
-		int c=a+b;
-		
+		int c=a+b;	
 		System.out.println("in Parent int addition is "+c);
-	}
-	
-	
-	
-	
+	}	
 }
 
 class Child extends Parent{
 	
-public void add(double a,double b) {
+public void add(double a,double b) {//child inheritance parent
 		
 		double c;
-		
 		c=a+b;
-		
 		System.out.println("in parent double addition is "+c);
 
 	}
 }
 
-
-
 public class Multiple {
 
 	public static void main(String[] args) {
 		
-		Child ch=new Child();
-		
+		Child ch=new Child();//object child class	
 		ch.add(2, 3);
 		ch.add(5.38,4.75);
 		
-
 	}
 
 }
